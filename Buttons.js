db299c63e1c65344ae990c16ae8e872327165bff
MAP01 <167375195+10PAM@users.noreply.github.com>
2024-10-19 20:01:04 -0700

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -1,4 +1,7 @@
 var previous_ID = null;
+var previous_ID_Project = null;
+
+//Handle Menu Buttons' Inputs
 function myFunction(typ3) {
     if (previous_ID !== null && typ3 !== previous_ID)
     {
@@ -6,8 +9,32 @@ function myFunction(typ3) {
         x_previous.style.display = "none";
     }
 
+    //Hide Previous visible project if not null
+    if (previous_ID_Project !== null && typ3 !== previous_ID_Project)
+    {
+        var proj_previous = document.getElementById(previous_ID_Project);
+        proj_previous.style.display = "none";
+    }
+
     var x = document.getElementById(typ3);
     previous_ID = typ3;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  //Handle Project Buttons' Inputs
+  function myFunctionProject(typ3) {
+    if (previous_ID_Project !== null && typ3 !== previous_ID_Project)
+    {
+        var x_previous = document.getElementById(previous_ID_Project);
+        x_previous.style.display = "none";
+    }
+    
+    var x = document.getElementById(typ3);
+    previous_ID_Project = typ3;
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
