a70590c0940a8222c386bc3f727e3982bd3b6e41
MAP01 <maguilera131113@gmail.com>
2026-07-11 15:18:11 -0700

    Update Buttons.js

--- mario/Buttons.js
+++ mario/Buttons.js
@@ -92,8 +92,10 @@ function myFunction(typ3) {
     // Open or Close Desired Menu
     if (x.style.display === "none") {
         x.style.display = "block";
-    } else {
+    } else if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
   }
 
